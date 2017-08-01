58f0fbc8ad3a678f54e1c3e44ea40de9566f0940
Daniel Grimshaw <danielgrimshaw@berkeley.edu>
2017-07-31 20:17:15 -0700

    Add license text

--- fizzBuzz.c
+++ fizzBuzz.c
@@ -1,7 +1,20 @@
 /*
- * Hyper FizzBuzz
+ * Hyper FizzBuzz (fizzBuzz.c)
  *
- * Copyright (c) 2017 Daniel Grimshaw <danielgrimshaw@berkeley.edu>
+ * fizzBuzz.c Copyright (C) 2017 Daniel Grimshaw <danielgrimshaw@berkeley.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * The fastest "readable" implementation of the simple programming challenge
  * FizzBuzz
